3a6cf0ea7cd6de80146ce3c59f4ba7bf49698ded
moist-potato <hugocpapi@gmail.com>
2024-11-01 01:46:32 -0300

    yui

---
+++ src/main/java/me/helena/bakingPlugin/BakingPlugin.java
@@ -0,0 +1,17 @@
+package me.helena.bakingPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BakingPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
