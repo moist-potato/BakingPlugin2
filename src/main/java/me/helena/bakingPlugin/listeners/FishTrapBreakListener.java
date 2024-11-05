450876da8b37e491ac0240647a92f9ce6a4dc4b4
moist-potato <hugocpapi@gmail.com>
2024-11-04 23:17:50 -0300

    a

--- src/main/java/me/helena/bakingPlugin/listeners/FishTrapBreakListener.java
+++ src/main/java/me/helena/bakingPlugin/listeners/FishTrapBreakListener.java
@@ -1,12 +1,16 @@
 package me.helena.bakingPlugin.listeners;
 
 import me.helena.bakingPlugin.utils.EntityTagUtil;
+import me.helena.bakingPlugin.utils.ItemUtils;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
@@ -19,13 +23,16 @@ public class FishTrapBreakListener implements Listener {
             Player player = (Player) event.getDamager();
 
 
-            List<Entity> nearbyEntities = player.getNearbyEntities(0.5, 0.5, 0.5);
+            List<Entity> nearbyEntities = player.getNearbyEntities(3.5, 3.5, 3.5);
 
             for(Entity entity: nearbyEntities){
 
                 if (EntityTagUtil.entityHasTag(entity, "exists") && entity instanceof ItemDisplay && EntityTagUtil.getIntFromTag(entity, "exists") == EntityTagUtil.getIntFromTag(event.getEntity(), "exists")){
+
+                    Location location = event.getEntity().getLocation();
                     event.getEntity().remove();
                     entity.remove();
+                    player.getWorld().dropItem(location, ItemUtils.CreateCustomItem(new ItemStack(Material.IRON_BARS), "&fFishtrap", "", false));
                 }
 
             }
